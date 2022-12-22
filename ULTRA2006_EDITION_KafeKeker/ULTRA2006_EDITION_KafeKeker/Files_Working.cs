73788ca58eeaaeb44e02bc56f1a492cfd9ba930f
topotototty <112858195+topotototty@users.noreply.github.com>
2022-12-22 11:13:52 +0300

    Add files via upload

---
+++ ULTRA2006_EDITION_KafeKeker/ULTRA2006_EDITION_KafeKeker/Files_Working.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ULTRA2006_EDITION_KafeKeker
+{
+    static class Files_Working
+    {
+        static public void SaveToFile<T>(T list, string path)
+        {
+            if (!File.Exists(Environment.SystemDirectory + "/" + path))
+            {
+                FileStream fileStream = File.Create(Environment.SystemDirectory + "/" + path);
+                fileStream.Dispose();
+            }
+
+            string json = JsonConvert.SerializeObject(list);
+            File.WriteAllText(Environment.SystemDirectory + "/" + path, json);
+        }
+
+        static public List<User> ReadUsersFromFile()
+        {
+            List<User> users;
+            if (!File.Exists(Environment.SystemDirectory + "/" + "Users.json"))
+            {
+                FileStream fileStream = File.Create(Environment.SystemDirectory + "/" + "Users.json");
+                fileStream.Dispose();
+
+                users = new List<User>();
+                users.Add(new User(0, 0, "Admin", "Admin"));
+                SaveToFile(users, Environment.SystemDirectory + "/" + "Users.json");
+            }
+            else
+            {
+                string usersInfo = File.ReadAllText(Environment.SystemDirectory + "/" + "Users.json");
+                users = JsonConvert.DeserializeObject<List<User>>(usersInfo);
+            }
+            return users;
+        }
+        static public List<T> ReadFromFile<T>(string path)
+        {
+            List<T> result;
+            if (!File.Exists(Environment.SystemDirectory + "/" + path))
+            {
+                FileStream fileStream = File.Create(Environment.SystemDirectory + "/" + path);
+                fileStream.Dispose();
+            }
+
+            string resultInfo = File.ReadAllText(Environment.SystemDirectory + "/" + path);
+            result = JsonConvert.DeserializeObject<List<T>>(resultInfo);
+
+            return result;
+        }
+    }
+}
